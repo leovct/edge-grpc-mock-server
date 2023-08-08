c4e94800ce56224ba313baf95bead79b8c3ea879
leovct <l009.vincent@gmail.com>
2023-08-08 20:59:42 +0200

    test: simplify test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,22 +4,6 @@
 # Make sure the server is started!
 # go run main.go --http-port 8080 --http-save-endpoint /save --grpc-port 8546 --output-dir out
 
-wait_for_service() {
-  port=$1
-  name=$2
-  {
-    while ! echo -n > /dev/tcp/127.0.0.1/$port; do
-      echo "$name port is not open yet. Waiting for 5 seconds"
-      sleep 5
-    done
-  } 2>/dev/null
-  echo "$name port is now open."
-}
-
-echo "Waiting for services..."
-wait_for_service 8546 "gRPC"
-wait_for_service 8080 "HTTP"
-
 echo "Sending gRPC requests..."
 grpcurl -plaintext  127.0.0.1:8546 v1.System/GetStatus	
 grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/BlockByNumber
