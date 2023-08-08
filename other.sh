97bc452a11c0f5504dae8cc398bb81c5f26032b2
leovct <l009.vincent@gmail.com>
2023-08-08 20:32:16 +0200

    test

--- other.sh
+++ other.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 git --no-pager diff
-if [[ $(git diff --quiet HEAD) ]]; then
+
+if [[ $(git status --porcelain) ]]; then
+  echo "❌ Error: gRPC service is not up to date. Please run 'make gen' and commit your changes."
+  exit 1
+else
+  echo "✅ gRPC service is up to date."
+fi
+
+if [[ $(git diff --quiet && git diff --cached --quiet) ]]; then
   echo "❌ Error: gRPC service is not up to date. Please run 'make gen' and commit your changes."
   exit 1
 else
