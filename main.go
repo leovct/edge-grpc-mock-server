f1a79fa6fdf7ea65133c0712956ddafa264f9f86
leovct <l009.vincent@gmail.com>
2023-08-08 10:07:35 +0200

    feat: add flags for grpc and http ports and output directory

--- main.go
+++ main.go
@@ -4,20 +4,37 @@ import (
 	"log"
 	"zero-provers/server/grpc"
 	"zero-provers/server/http"
+
+	"github.com/spf13/cobra"
 )
 
-const (
-	gRPCServerPort  = 8546
-	HTTPServerPort  = 8080
-	ProofsOutputDir = "out"
-)
+type Config struct {
+	GRPCServerPort  int
+	HTTPServerPort  int
+	ProofsOutputDir string
+}
 
 func main() {
-	// Start the gRPC server.
-	go func() {
-		log.Fatal(grpc.StartgRPCServer(gRPCServerPort), nil)
-	}()
+	var config Config
+	var rootCmd = &cobra.Command{
+		Use:   "mock",
+		Short: "Edge gRPC mock server",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Start the gRPC server.
+			go func() {
+				log.Fatal(grpc.StartgRPCServer(config.GRPCServerPort))
+			}()
 
-	// Start the HTTP server.
-	log.Fatal(http.StartHTTPServer(HTTPServerPort, ProofsOutputDir), nil)
+			// Start the HTTP server.
+			log.Fatal(http.StartHTTPServer(config.HTTPServerPort, config.ProofsOutputDir))
+		},
+	}
+
+	// Define flags for configuration
+	rootCmd.Flags().IntVar(&config.GRPCServerPort, "grpc", 8546, "gRPC server port")
+	rootCmd.Flags().IntVar(&config.HTTPServerPort, "http", 8080, "HTTP server port")
+	rootCmd.Flags().StringVarP(&config.ProofsOutputDir, "output-dir", "o", "out", "Proofs output directory")
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
