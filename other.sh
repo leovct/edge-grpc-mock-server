786f9154b5067ceb5d41dae73c5ce7d2d1e8e1b4
leovct <l009.vincent@gmail.com>
2023-08-08 20:22:45 +0200

    test

---
+++ other.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $(git status --porcelain) ]]; then
+  echo "❌ Error: gRPC service is not up to date. Please run 'make gen' and commit your changes."
+  exit 1
+else
+  echo "✅ gRPC service is up to date."
+fi
