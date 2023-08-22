3ce211f9f9138b853191da9291b2e951a17bc09e
Léo Vincent <28714795+leovct@users.noreply.github.com>
2023-08-22 13:35:23 +0200

    feat: new gRPC `UpdateTrace` method to update trace mock data (#23)
    * feat: add `UpdateTrace` gRPC method and update proto structs
    
    * test: update test script to check that the update feature works well
    
    * chore: add `out` folder
    
    * chore: remove huge strings and byte arrays from `README`
    
    * doc: add instructions to update trace mock data

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+set -x
 
 # Send a few gRPC/HTTP requests to the mock server.
 # Make sure the server is started!
 # go run main.go --http-port 8080 --http-save-endpoint /save --grpc-port 8546 --output-dir out
-
 echo "Sending gRPC requests..."
-grpcurl -plaintext  127.0.0.1:8546 v1.System/GetStatus	
-grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/BlockByNumber
-grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace
+grpcurl -plaintext  127.0.0.1:8546 v1.System/GetStatus | jq
+grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/BlockByNumber | jq
+
+# Get trace
+grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace | jq
+grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace | jq -r .trace | base64 -d | jq
+
+# Update trace
+grpcurl -plaintext -d "{\"trace\": $(cat data/trace1.json | jq .trace)}" 127.0.0.1:8546 v1.System/UpdateTrace | jq
+grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace | jq
+grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace | jq -r .trace | base64 -d | jq
 
 echo "Sending HTTP requests..."
 curl -s -X POST -H "Content-Type: application/json" -d '{"name": "salamander", "type": "fire"}' http://127.0.0.1:8080/save
