e18b2986b712d2bddd5d9712db0865b3327a5034
leovct <l009.vincent@gmail.com>
2023-08-24 19:56:24 +0200

    ci: remove job

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Send gRPC requests to the mock server in a loop.
-for ((i=1; i<=20; i++)); do
+for ((i = 1; i <= $1; i++)); do
     grpcurl -plaintext 127.0.0.1:8546 v1.System/GetStatus | jq
     grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/BlockByNumber | jq
     grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace | jq
