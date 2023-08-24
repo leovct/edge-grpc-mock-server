4d5711c35879a93108d56e728efffa16d9b83a82
leovct <l009.vincent@gmail.com>
2023-08-24 19:18:24 +0200

    chore: quick update

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Send gRPC requests to the mock server in a loop.
-n=100
+n=$1
 for ((i=1; i<=$n; i++)); do
-    grpcurl -plaintext 127.0.0.1:8546 v1.System/GetStatus | jq
-    grpcurl -plaintext 127.0.0.1:8546 v1.System/GetStatus | jq
     grpcurl -plaintext 127.0.0.1:8546 v1.System/GetStatus | jq
     grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/BlockByNumber | jq
     grpcurl -plaintext  -d '{"number": 1}' 127.0.0.1:8546 v1.System/GetTrace | jq
