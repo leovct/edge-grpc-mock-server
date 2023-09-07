6ca21abe8f64ab3ee56f321a7fa6157e55abdb5e
Léo Vincent <28714795+leovct@users.noreply.github.com>
2023-09-07 13:08:25 +0200

    chore: update config msgs (#42)

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -89,8 +89,8 @@ func StartgRPCServer(_config ServerConfig) error {
 	pb.RegisterSystemServer(s, &server{})
 
 	// Start serving incoming gRPC requests on the listener.
+	log.Debug().Msgf("gRPC server config: %+v", config)
 	log.Info().Msgf("gRPC server is listening on port %d", config.Port)
-	log.Debug().Msgf("Config: %+v", config)
 	if err := s.Serve(listener); err != nil {
 		log.Error().Err(err).Msg("Unable to start gRPC server")
 		return err
